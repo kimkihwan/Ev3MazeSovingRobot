82d980e9b1c10814f8ade38cb943d6c9f6037a08
kimkihwan <gunyoungwon@192.168.0.12>
2017-06-04 23:00:41 +0900

    init, light fuction

--- robot2/src/robot2/Colorsensor.java
+++ robot2/src/robot2/Colorsensor.java
@@ -3,30 +3,24 @@ import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.AbstractFilter;
 
 public class Colorsensor extends AbstractFilter{
-	public static final boolean BLACK = true;
-	public static final boolean WHITE = false;
-	
 	float[] sample;
 	public Colorsensor (SampleProvider source){
 		super(source);
 		sample = new float[sampleSize];
-		
 	}
-	public string get_color(){
+	public String get_color(){
 		super.fetchSample(sample,0);
-		string colorName = "";
+		String colorName = "";
 		switch((int)sample[0]){
-			case Color.NONE: colorName = "NONE"; break;//-1
-			case Color.BLACK: colorName = "BLACK"; break;//7
-			case Color.BLUE: colorName = "BLUE"; break;//2
-			case Color.GREEN: colorName = "GREEN"; break;//1
-			case Color.YELLOW: colorName = "YELLOW"; break;//3
-			case Color.RED: colorName = "RED"; break;//0
-			case Color.WHITE: colorName = "WHITE"; break;//6
-			case Color.BROWN: colorName = "BROWN"; break;//13
+			case -1: colorName = "NONE"; break;//-1
+			case 7: colorName = "BLACK"; break;//7
+			case 2: colorName = "BLUE"; break;//2
+			case 1: colorName = "GREEN"; break;//1
+			case 3: colorName = "YELLOW"; break;//3
+			case 0: colorName = "RED"; break;//0
+			case 6: colorName = "WHITE"; break;//6
+			case 13: colorName = "BROWN"; break;//13
 		}
 		return colorName;
 	}
-	
-	
 }
--- robot2/src/robot2/hello.java
+++ robot2/src/robot2/hello.java
@@ -1,25 +1,55 @@
 package robot2;
 
-
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-import lejos.hardware.*;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+//import lejos.hardware.Sound;
+
+//import lejos.hardware.sensor.SensorMode;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+//import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.port.Port;
+//import lejos.hardware.port.SensorPort;
+
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
 
 public class hello {
+	public static final boolean BLACK = true;
+	public static final boolean WHITE = false;
+	
 	RemoteEV3 ev3;
 	static RMIRegulatedMotor motorA;
 	static RMIRegulatedMotor motorB;
+
+	static Lightsensor light;
+	static Colorsensor color;
+	//static Sonarsensor sonar;
+	static Brick b;
+	
+	static Port s2;
+	static Port s3;
+	static Port s4;
+	
 	
 	
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		//Creates the remote ev3
 		RemoteEV3 ev3=new RemoteEV3("10.0.1.1");
+		
 		ev3.isLocal();
 		
+		init(ev3);
+		
+		light();
+		
+	}
+
+	public static void init(RemoteEV3 ev3) {	
 		//gets ports
 		ev3.getPort("A");
 		ev3.getPort("B");
@@ -28,26 +58,31 @@ public class hello {
 		if(motorA==null){motorA = ev3.createRegulatedMotor("A", 'L');}
 		if(motorB==null){motorB = ev3.createRegulatedMotor("B", 'L');}
 		
-		//lets move motors
-		motorB.forward();
-		motorA.forward();
-		try{
-			Thread.sleep(5000);
-		}
-		catch(InterruptedException e){
-			e.printStackTrace();
-		}
-		motorB.stop(true);
-		motorA.stop(true);
+		b = BrickFinder.getDefault();
 		
-		//beep because its nice to know we are done
-		Sound.twoBeeps();
+		s2 = b.getPort("S2");
+		s3 = b.getPort("S3");
+		s4 = b.getPort("S4");
 		
-		//close ports. Must be completed every time.
-		motorB.close();
-		motorA.close();
+		//EV3UltrasonicSensor EV3sonar = new EV3UltrasonicSensor(s2); 
+		NXTLightSensor NXTlight = new NXTLightSensor(s3);
+		EV3ColorSensor EV3color = new EV3ColorSensor(s4);
 		
+	//	sonar = new Sonarsensor(EV3sonar);
+		light = new Lightsensor(NXTlight);
+		color = new Colorsensor(EV3color);
 	}
-
+	
+	public static void light() throws RemoteException {
+		if(light.get_light()==BLACK) {
+			motorB.forward();
+			motorA.forward();
+		}
+		else if(light.get_light()==WHITE) {
+			motorB.stop(true);
+			motorA.stop(true);
+		}
+	}
+	
 }
 
