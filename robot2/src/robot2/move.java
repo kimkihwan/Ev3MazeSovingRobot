30c76373a28502e94ea7084a28937085af5bf9e6
kihwa <kihwa@DESKTOP-A43ATC9>
2017-06-12 22:59:03 +0900

    123

--- robot2/src/robot2/move.java
+++ robot2/src/robot2/move.java
@@ -7,6 +7,7 @@ import java.rmi.RemoteException;
 
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
+import lejos.hardware.Bluetooth;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.SensorMode;
 import lejos.hardware.sensor.EV3ColorSensor;
@@ -16,9 +17,12 @@ import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
+import lejos.remote.nxt.NXTConnection;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.PublishFilter;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class move {
@@ -28,14 +32,19 @@ public class move {
 	static int[] path=new int[4650007];
 	static int p=0;
 	
+	static NXTConnection btc = Bluetooth.getNXTCommConnector().waitForConnection(0, NXTConnection.RAW);
+	static DataInputStream input = btc.openDataInputStream();
+	static DataOutputStream output = btc.openDataOutputStream();
+	static int command=0;
 	public static void main(String[] args) throws RemoteException, MalformedURLException, IOException, NotBoundException {
-		mov();
+		mov();	
 	}
 	
 	static void mov() throws IOException, RemoteException, MalformedURLException, NotBoundException {
 		//Creates the remote ev3
 		RemoteEV3 ev3=new RemoteEV3("10.0.1.1");
-
+		Sound.beep();
+		LCD.drawString("Waiting!\n", 4, 4);
 		//gets ports
 		ev3.getPort("A");
 		ev3.getPort("B");
@@ -69,152 +78,156 @@ public class move {
 		int rot = 2;
 		int cnt_p=0;
 		int speed_flag=1;//original speed = 1, lower speed = 2
-		while(!Button.ESCAPE.isDown()) {
-			cnt++;
-            cnt_p++;                                        
-			if(cnt==50)
-			{
-				/*
-				//150정도면 한칸움직임
-				motorA.stop(false);
-				motorB.stop(false);
-				*/
-				LCD.scroll();
-				light = lightSensor.getRedMode();
-				float[] sample1 = new float[light.sampleSize()];
-				light.fetchSample(sample1, 0);
-				bright = (float)sample1[0];
-				
-				sonar.fetchSample(sample4, 0);
-				distance = sample4[0];
-				LCD.drawString(distance + " \n", 5, 5);
-				if(distance<0.03||distance>10000)
+		
+		try{
+		    command = input.readByte();
+			}catch(IOException e){
+			   System.exit(1);
+		}
+		if(command==1)
+		{
+			while(!Button.ESCAPE.isDown()) {
+				cnt++;
+	            cnt_p++;
+	            if(cnt==50)
 				{
-					motorA.stop(true);
-					motorB.stop(true);
-					color = colorSensor.getColorIDMode();
-					float[] sample3 = new float[color.sampleSize()];
-					color.fetchSample(sample3, 0);
-					colorID = (int)sample3[0];
-					colorName = "";
-					switch(colorID){
-					case -1: colorName = "NONE"; break;
-					case 0: colorName = "Red";
-										path[p]=2;
-										speed_flag=1;
-										motorA.setSpeed(180);
-										motorB.setSpeed(180);
-										motorB.stop(true);motorA.stop(true);
-										motorA.rotate(510,true);motorB.rotate(-510);
-										for(i=0;i<440;i++){motorA.forward();motorB.forward();}
-										motorB.stop(true);motorA.stop(true);
-										break;//left rotate
-					case 1: colorName = "Green"; break;
-					case 2: colorName = "Blue";
-										path[p]=3;
-										motorB.stop(true);motorA.stop(true);
-										motorA.rotate(-510,true);motorB.rotate(510);
-										speed_flag=1;
-										motorA.setSpeed(180);
-										motorB.setSpeed(180);
-										for(i=0;i<440;i++){motorA.forward();motorB.forward();}
-										motorB.stop(true);motorA.stop(true);
-										break;//right rotate
-					case 3: colorName = "Yellow";
-										path[p]=4;
-										motorB.stop(true);motorA.stop(true);motorA.rotate(510,true);motorB.rotate(-510);//left rotate
-										speed_flag=1;
-										motorA.setSpeed(180);
-										motorB.setSpeed(180);
-										for(i=0;i<440;i++){motorA.forward();motorB.forward();}
-										motorB.stop(true);motorA.stop(true);motorA.rotate(-510,true);motorB.rotate(510);//right rotate
-										for(i=0;i<440;i++){motorA.forward();motorB.forward();}
-										motorB.stop(true);motorA.stop(true);
-										break;
-					case 6: colorName = "White"; break;
-					case 7: colorName = "Black"; LCD.clearDisplay();LCD.drawString("FINISH I'm Solved!", 1, 1); break;
-					case 13:colorName = "Brown"; break;
-					}
-					LCD.drawString(colorID + " - " + colorName + " \n", 5, 5);
-					if(colorID==7)
-					{
-						Delay.msDelay(2000);
-						break;
-					}
-				}
-				else if(distance<0.3)
-				{
-					speed_flag=2;
-					motorA.setSpeed(80);
-					motorB.setSpeed(80);
-					motorA.forward();
-					motorB.forward();
-				}
-				else
-				{
-					motorA.forward();
-					motorB.forward();
-				}
-				
-				
-				if(bright>=0.35){
-					for(i=0;i<rot;i++){
-						motorA.rotate(-15*k,true);
-						motorB.rotate(15*k);
+	            	command = input.available();
+	                if(command==2)
+	                {
+	                	motorA.stop(true);
+	                	motorB.stop(true);
+	                }
+	                else
+	                {
+	                	//150정도면 한칸움직임
+						LCD.scroll();
 						light = lightSensor.getRedMode();
+						float[] sample1 = new float[light.sampleSize()];
 						light.fetchSample(sample1, 0);
-						bright2 = (float)sample1[0];
-						if(bright2<0.35)
+						bright = (float)sample1[0];
+						
+						sonar.fetchSample(sample4, 0);
+						distance = sample4[0];
+						//LCD.drawString(distance + " \n", 5, 5);
+						if(distance<0.03||distance>10000)
 						{
+							motorA.stop(true);
+							motorB.stop(true);
+							color = colorSensor.getColorIDMode();
+							float[] sample3 = new float[color.sampleSize()];
+							color.fetchSample(sample3, 0);
+							colorID = (int)sample3[0];
+							colorName = "";
+							switch(colorID){
+							case -1: colorName = "NONE"; break;
+							case 0: colorName = "Red";
+												path[p]=2;
+												speed_flag=1;
+												motorA.setSpeed(180);
+												motorB.setSpeed(180);
+												motorB.stop(true);motorA.stop(true);
+												motorA.rotate(510,true);motorB.rotate(-510);
+												for(i=0;i<400;i++){motorA.forward();motorB.forward();}
+												motorB.stop(true);motorA.stop(true);
+												break;//left rotate
+							case 1: colorName = "Green"; break;
+							case 2: colorName = "Blue";
+												path[p]=3;
+												motorB.stop(true);motorA.stop(true);
+												motorA.rotate(-510,true);motorB.rotate(510);
+												speed_flag=1;
+												motorA.setSpeed(180);
+												motorB.setSpeed(180);
+												for(i=0;i<400;i++){motorA.forward();motorB.forward();}
+												motorB.stop(true);motorA.stop(true);
+												break;//right rotate
+							case 3: colorName = "Yellow";
+												path[p]=4;
+												motorB.stop(true);motorA.stop(true);motorA.rotate(510,true);motorB.rotate(-510);//left rotate
+												speed_flag=1;
+												motorA.setSpeed(180);
+												motorB.setSpeed(180);
+												for(i=0;i<400;i++){motorA.forward();motorB.forward();}
+												motorB.stop(true);motorA.stop(true);motorA.rotate(-510,true);motorB.rotate(510);//right rotate
+												for(i=0;i<400;i++){motorA.forward();motorB.forward();}
+												motorB.stop(true);motorA.stop(true);
+												break;
+							case 6: colorName = "White"; break;
+							case 7: colorName = "Black"; LCD.clearDisplay();LCD.drawString("FINISH I'm Solved!", 1, 1); break;
+							case 13:colorName = "Brown"; break;
+							}
+							LCD.drawString(colorID + " - " + colorName + " \n", 5, 5);
+							if(colorID==7)
+							{
+								motorB.close();
+								motorA.close();
+								colorSensor.close();
+								lightSensor.close();
+								sonarSensor.close();
+								Delay.msDelay(2000);
+								break;
+							}
+						}
+						else if(distance<0.3)
+						{
+							speed_flag=2;
+							motorA.setSpeed(80);
+							motorB.setSpeed(80);
+							motorA.forward();
+							motorB.forward();
+						}
+						else
+						{
+							motorA.forward();
+							motorB.forward();
+						}
+						
+						
+						if(bright>=0.35){
+							for(i=0;i<rot;i++){
+								motorA.rotate(-20*k,true);
+								motorB.rotate(20*k);
+								light = lightSensor.getRedMode();
+								light.fetchSample(sample1, 0);
+								bright2 = (float)sample1[0];
+								if(bright2<0.35)
+								{
+									motorB.forward();
+									motorA.forward();
+									rot=2;
+									break;
+								}
+							}
+							rot*=2;
+							k*=-1;
+							
+						}
+						else{
+							path[p] = 1;
 							motorB.forward();
 							motorA.forward();
-							rot=2;
-							break;
+							k=1;
 						}
-					}
-					rot*=2;
-					k*=-1;
-					
+						
+						cnt = 0;		
+						//LCD.drawString(bright + " \n" + bright2 + " \n", 1, 1);
+	                }//else command==1,3
+				}//cnt==50
+				if(speed_flag==1){
+					cnt_p++;
 				}
-				else{
-					path[p] = 1;
-					motorB.forward();
-					motorA.forward();
-					k=1;
+				if(cnt_p==5650){//cnt_p = beep five times in original speed, 11 times in lower speed.
+					Sound.beep();
+					LCD.drawString(path[p] + " send\n", 4, 4);
+					output.writeByte(path[p]);
+					output.flush();
+					p++;
+					cnt_p=0;
 				}
-				
-				cnt = 0;		
-				//LCD.drawString(bright + " \n" + bright2 + " \n", 1, 1);
-				
-			
-			}
-			if(speed_flag==1){
-				cnt_p++;
-			}
-			if(cnt_p==5650){//cnt_p = beep five times in original speed, 11 times in lower speed.
-				p++;
-				Sound.beep();
-				cnt_p=0;
-			}
-		}
-		
-		/*		
-		//lets move motors
-		motorB.forward();
-		motorA.forward();
-		try{
-			Thread.sleep(5000);
-		}
-		catch(InterruptedException e){
-			e.printStackTrace();
-		}
-		motorB.stop(true);
-		motorA.stop(true);
-		
-		//beep because its nice to know we are done
-		Sound.twoBeeps();
-		*/
-		//close ports. Must be completed every time.
+			}//while
+		}//command==1
+		input.close();
+		output.close();
 		motorB.close();
 		motorA.close();
 		colorSensor.close();
--- robot2/src/robot2/zxc.java
+++ robot2/src/robot2/zxc.java
@@ -1,30 +1,55 @@
 package robot2;
 
 import lejos.hardware.Bluetooth;
+import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.remote.nxt.NXTConnection;
+
 import java.io.*;
 
 public class zxc {
-	public static void main(String args[]){
-			char command = 'a';
+	public static void main(String args[]) throws IOException{
+			byte command = 0;
+			int i=1;
+			//int[] path = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+			//int[] path2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 			LCD.drawString("Waiting!\n", 4, 4);
 			LCD.scroll();
 			NXTConnection btc = Bluetooth.getNXTCommConnector().waitForConnection(0, NXTConnection.RAW);
 			Sound.beep();
 			LCD.drawString("Connect success!\n", 4, 4);
 			LCD.scroll();
-			DataInputStream dis = btc.openDataInputStream();
+			
+			DataInputStream input = btc.openDataInputStream();
+			DataOutputStream output = btc.openDataOutputStream();
 			while(true){
 			    try{
-			        command = dis.readChar();
-					}catch(IOException e){
-					    System.exit(1);
-					}
-			    LCD.drawString(command + " \n", 5, 5);
-				LCD.scroll();
-				}//while
-		}//main
+			       command = input.readByte();
+				}catch(IOException e){
+				   System.exit(1);
+				}
+			    
+				LCD.drawString(command + " \n", 4, 4);
+				if(Button.waitForAnyPress()==1){
+					LCD.scroll();
+					//for(i=0;i<16;i++)
+					output.writeByte(i);
+					//output.writeByte(i);
+					//path2[i]+=10;
+					//output.writeInt(path2[i]);
+					//LCD.drawString(path[i]+ " and "+ path2[i] + " send\n", 4, 4);
+					LCD.drawString(Integer.toString(i)+" send\n", 4, 4);
+					LCD.scroll();
+					i++;
+					output.flush();
+					if(Button.waitForAnyPress()==2)
+						break;
+				}
+			}//while
+			input.close();
+			output.close();
+			btc.close();
+	}//main
 }//class	
 
